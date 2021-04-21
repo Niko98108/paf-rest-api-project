c557b5c51a24fd531b1ac2d793092960888d8885
Nisal <iamnisal123@gmail.com>
2021-04-21 14:13:53 +0530

    developed manager service insert oparation

--- src/com/project/ManagerService.java
+++ src/com/project/ManagerService.java
@@ -1,19 +1,49 @@
-//package com.project;
-//
-////for Rest Services
-//import javax.ws.rs.*;
-//import javax.ws.rs.core.MediaType;
-//@Path("/Manager")
-//public class ManagerService {
-//	// Read Data
-//		@GET
-//		@Path("/")
-//		@Produces(MediaType.TEXT_HTML)
-//		
-//		public String readItems() {
-//
-//			return "Hello Manager";
-//		}
-//
-//
-//}
+package com.project;
+import model.Manager;
+
+//for Rest Services
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+//for XML
+//import org.jsoup.*;
+//import org.jsoup.parser.*;
+//import org.jsoup.nodes.Document;
+
+@Path("/Manager")
+public class ManagerService {
+	
+	Manager managerObj = new Manager();
+	// Read Data
+		@GET
+		@Path("/")
+		@Produces(MediaType.TEXT_HTML)
+		
+		public String readItems() {
+
+			return "Hello Manager";
+		}
+// Insert Manager Details
+		@POST
+		@Path("/")
+		@Consumes(MediaType.APPLICATION_JSON)
+		@Produces(MediaType.TEXT_PLAIN)
+		public String insertItem(String managerData) {
+				//convert the input string to a JSON object
+				JsonObject itemObject = new JsonParser().parse(managerData).getAsJsonObject();
+				
+				//Read the values from the JSON object
+				
+				String managerName = itemObject.get("managerName").getAsString();
+				String email= itemObject.get("email").getAsString();
+				String managerType = itemObject.get("managerType").getAsString();
+				String phoneNo = itemObject.get("phoneNo").getAsString();
+				
+				{ 
+				 String output = managerObj.insertManager(managerName, email, managerType, phoneNo); 
+				return output; 
+				}
+		
+		}
+}
---
+++ src/model/Manager.java
@@ -0,0 +1,212 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Manager {
+	public Connection getconnection() {
+		
+		Connection con = null;
+
+		 try
+		 {
+		 Class.forName("com.mysql.cj.jdbc.Driver");
+		 con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf_rest_api","root", "");
+		 
+		 //For testing
+
+		 System.out.print("Successfully connected to Database");
+		 }
+		 catch(Exception e){
+		 e.printStackTrace();
+		 }
+
+		 return con;
+	}
+	public String insertManager(String managerName, String email,String managerType, String phoneNo)
+	{
+	 String output = "";
+	try
+	 {
+	 Connection con = getconnection();
+	 if (con == null)
+	 {
+	 return "Error while connecting to the database";
+	 }
+	 // create a prepared statement
+	 String query = " INSERT INTO paf_rest_api.projectmanager(`manager_id`,`managerName`,`email`,`managerType`,`mobile`)"
+	 + " values (?, ?, ?, ?, ?)";
+	 PreparedStatement preparedStmt = con.prepareStatement(query);
+	 // binding values
+	 preparedStmt.setInt(1, 0);
+	 preparedStmt.setString(2, managerName);
+	 preparedStmt.setString(3, email);
+	 preparedStmt.setString(4, managerType);
+	 preparedStmt.setString(5,phoneNo);// preparedStmt.setDouble(4, Double.parseDouble(price));
+	
+	
+	//execute the statement
+	 preparedStmt.execute();
+	 con.close();
+	 output = "successfully Inserted to ProjectManager Table";
+	 }
+	catch (Exception e)
+	 {
+	 output = "Error While Inserting";
+	 System.err.println(e.getMessage());
+	 e.printStackTrace();
+	 }
+	return output;
+	}
+	
+	//update Project data function
+	public String updateProject(String projectId,String projectName, String projectDesc,String projectType, String managerId, String startDate,String endDate)
+	{
+	 String output = "";
+	try
+	 {
+	 Connection con = getconnection();
+	 if (con == null)
+	 {
+	 return "Error while connecting to the database";
+	 }
+	 // create a prepared statement
+	 String query = "UPDATE project SET  projectName=?,  projectDesc=?, projectType=?, manager_id=?, startDate=? ,endDate=? WHERE project_id='"+projectId+"' ";
+	 PreparedStatement preparedStmt = con.prepareStatement(query);
+	 // binding values
+	 
+	 preparedStmt.setString(1, projectName);
+	 preparedStmt.setString(2, projectDesc);
+	 preparedStmt.setString(3, projectType);
+	 preparedStmt.setInt(4, Integer.parseInt(managerId));
+	 preparedStmt.setString(5, startDate);
+	 preparedStmt.setString(6, endDate); 
+	
+	 
+	//execute the statement
+	 preparedStmt.execute();
+	 con.close();
+	 output = "Successfully Updated Project Table";
+	 }
+	catch (Exception e)
+	 {
+	 output = "Error while updating";
+//	 System.err.println(e.getMessage());
+	 e.printStackTrace();
+	 }
+	return output;
+	}
+	
+	//Delete project data
+	public String deleteProject(String projectId)
+	{
+	 String output = "";
+	try
+	 {
+	 Connection con = getconnection();
+	 if (con == null)
+	 {
+	 return "Error while connecting to the database";
+	 }
+	//create a prepared statement
+	 String query = "DELETE FROM project WHERE project_id=?" ;
+	 
+	 PreparedStatement preparedStatement = con.prepareStatement(query);
+	 
+	 //binding Values
+	 preparedStatement.setInt(1, Integer.parseInt(projectId));
+	 
+	 //execute the statement
+	 
+	 preparedStatement.execute();
+	 
+	 con.close();
+	 output = "Successfully Deleted from Project Table";
+
+	 
+	 }
+	catch (Exception e)
+	 {
+	 output = "Error while Deleting";
+	 System.err.println(e.getMessage());
+	 e.printStackTrace();
+	 }
+	return output;
+	}
+	
+	//Read Project Data
+	public String readProject()
+	{
+	 String output = "";
+	try
+	 {
+	 Connection con = getconnection();
+	 if (con == null)
+	 {
+	 return "Error while connecting to the Database.";
+	 }
+	 // Prepare the HTML table to be displayed
+	 
+//	 output = "<table border='1'><tr><th>Item Code</th>"
+//	 +"<th>Item Name</th><th>Item Price</th>"
+//	 + "<th>Item Description</th>"
+//	 + "<th>Update</th><th>Remove</th></tr>";
+	 
+	 output = "<table style=\"border-collapse: collapse;border: 1px solid black\">\r\n"
+	 		+ "  <thead>\r\n"
+	 		+ "    <tr style=\"border: 1px solid black;background-color: lightgray\">\r\n"
+	 		+ "      <th style=\"border: 1px solid black\" scope=\"col\">Project ID</th>\r\n"
+	 		+ "      <th style=\"border: 1px solid black\"scope=\"col\">Project Name</th>\r\n"
+	 		+ "      <th style=\"border: 1px solid black\"scope=\"col\">Project Descripton</th>\r\n"
+	 		+ "      <th style=\"border: 1px solid black\"scope=\"col\">Project Type</th>\r\n"
+	 		+ "      <th style=\"border: 1px solid black\"scope=\"col\">Project Manager ID</th>\r\n"
+	 		+ "      <th style=\"border: 1px solid black\"scope=\"col\">Start Data</th>\r\n"
+	 		+ "      <th style=\"border: 1px solid black\"scope=\"col\">End Data</th>\r\n"
+	 		+ "    </tr>\r\n"
+	 		+ "  </thead>\r\n"
+	 		+ "</table";
+	 
+	 String query = "SELECT * FROM project";
+	 
+	 Statement stmt = con.createStatement();
+	 ResultSet rs = stmt.executeQuery(query);
+	 
+	 // iterate through the rows in the result set
+	 while (rs.next())
+	 {
+	 String projectId = Integer.toString(rs.getInt("project_id"));
+	 String projectName = rs.getString("projectName");
+	 String projectDesc = rs.getString("projectDesc");
+	 String projectType = rs.getString("projectType") ;
+	 String managerId = rs.getString("manager_id");
+	 String startDate = rs.getString("startDate");
+	 String endDate = rs.getString("endDate");
+	 
+	 // Add a row into the HTML table
+	 output += "<tr style=\"border: 1px solid black\"><td>" + projectId + "</td>";
+	 output += "<td style=\"border: 1px solid black\">" + projectName + "</td>";
+	 output += "<td style=\"border: 1px solid black\">" + projectDesc + "</td>"; 
+	 output += "<td style=\"border: 1px solid black\">" + projectType + "</td>"; 
+	 output += "<td style=\"border: 1px solid black\">" + managerId + "</td>"; 
+	 output += "<td style=\"border: 1px solid black\">" + startDate + "</td>"; 
+	 output += "<td style=\"border: 1px solid black\">" + endDate + "</td>";
+
+	 }
+	 con.close();
+	 // Complete the HTML table
+	 output += "</table>";
+	 }
+	catch (Exception e)
+	 {
+	 output = "Error while reading the items.";
+	 System.err.println(e.getMessage());
+	 e.printStackTrace();
+	 }
+	return output;
+	}
+
+
+}
