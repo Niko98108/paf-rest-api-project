902db527d4ba59284dbfd7b751c9eeef487c8c72
Nisal <iamnisal123@gmail.com>
2021-04-21 10:10:28 +0530

    test manager service

---
+++ src/com/project/ManagerService.java
@@ -0,0 +1,19 @@
+//package com.project;
+//
+////for Rest Services
+//import javax.ws.rs.*;
+//import javax.ws.rs.core.MediaType;
+//@Path("/Manager")
+//public class ManagerService {
+//	// Read Data
+//		@GET
+//		@Path("/")
+//		@Produces(MediaType.TEXT_HTML)
+//		
+//		public String readItems() {
+//
+//			return "Hello Manager";
+//		}
+//
+//
+//}
